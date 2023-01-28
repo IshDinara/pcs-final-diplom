2a8a79663f802158ac1f8001d6e5a56b7ffadf44
Dinara Andrianova <ish.dinara@gmail.com>
2023-01-28 20:14:43 +0300

    add constructor BooleanSearchEngine

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,11 +1,48 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
+    Map<String, List<PageEntry>> listWords = new HashMap<>();
     //???
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
+        if (pdfsDir.isDirectory()) {
+            for (File item: pdfsDir.listFiles()) {
+                var doc = new PdfDocument(new PdfReader(item));
+                // TODO исправить счетчик
+                for (int i = 1; i < 3; i++) {
+                    var text = PdfTextExtractor.getTextFromPage(doc.getPage(i));
+                    var words = text.split("\\P{IsAlphabetic}+");
+                    Map<String, Integer> freqs = new HashMap<>();
+                    for (var word : words) {
+                        if (word.isEmpty()) {
+                            continue;
+                        }
+                        word = word.toLowerCase();
+                        freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                    }
+                    for (Map.Entry<String, Integer> mp : freqs.entrySet()) {
+                        if (!listWords.containsKey(mp.getKey())) {
+                            PageEntry pageEntry = new PageEntry(item.getName(), i, mp.getValue());
+                            List<PageEntry> pageEntryList = new ArrayList<>();
+                            pageEntryList.add(pageEntry);
+                            listWords.put(mp.getKey(), pageEntryList);
+                        } else {
+                            PageEntry pageEntry = new PageEntry(item.getName(), i, mp.getValue());
+                            List<PageEntry> pageEntryList = listWords.get(mp.getKey());
+                            pageEntryList.add(pageEntry);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(listWords);
+        System.out.println();
         // прочтите тут все pdf и сохраните нужные данные,
         // тк во время поиска сервер не должен уже читать файлы
     }
--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -3,5 +3,22 @@ public class PageEntry implements Comparable<PageEntry> {
     private final int page;
     private final int count;
 
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "pdfName " + pdfName + " page " + page + " count " + count;
+    }
+
+    // TODO исправить compareTo
+    @Override
+    public int compareTo(PageEntry o) {
+        return 0;
+    }
+
     // ???
 }
